c5a1556d7d243c4b0201b55c18d23a55f292a683
Mateus Teixeira Magalhães <mateus96mt@gmail.com>
2023-11-30 02:31:41 -0300

    Changing localhost to fixed ip address

--- task-manager-mongo-db-first-config/mongodb.js
+++ task-manager-mongo-db-first-config/mongodb.js
@@ -3,7 +3,7 @@ const { MongoClient } = require("mongodb");
 // import { MongoClient } from 'mongodb'
  
 // Connection URL
-const url = "mongodb://localhost:27017";
+const url = "mongodb://127.0.0.1:27017";
 const client = new MongoClient(url);
  
 // Database Name
