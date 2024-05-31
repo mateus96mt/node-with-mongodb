42aff35c8ba799d3a0cda847f5ffc67c18a65401
Mateus Teixeira Magalhães <mateus96mt@gmail.com>
2024-05-31 12:38:14 -0300

    filter tasks, pagination, sorting

--- src/routers/task.js
+++ src/routers/task.js
@@ -2,6 +2,7 @@ const express = require('express')
 const Task = require('../models/task')
 const auth = require('../middleware/auth')
 const router = new express.Router()
+const pageSize = 3
 
 router.post('/tasks', auth, async (req, res) => {
     const task = new Task({
@@ -18,8 +19,21 @@ router.post('/tasks', auth, async (req, res) => {
 })
 
 router.get('/tasks', auth, async (req, res) => {
+
+    let page = req.query.page ?? 1
+    let sortBy = req.query.sortBy?.split(':')[0] ?? 'createdAt'
+    let sortOrder = req.query.sortBy?.split(':')[1] === 'asc' ?? true
+
+    let filter = {
+        owner: req.user._id,
+    }
+    if(req.query.completed) filter["completed"] = req.query.completed === 'true';
+
+    let options = {skip: (page-1) * pageSize, limit: pageSize, sort: {}}
+    options["sort"][sortBy] = (sortOrder === true ? 1 : -1)
+
     try {
-        let tasks = await Task.find({owner: req.user._id})
+        let tasks = await Task.find(filter, null, options)
         res.send(tasks)
     } catch (e) {
         res.status(500).send()
@@ -52,7 +66,7 @@ router.patch('/tasks/:id', auth, async (req, res) => {
     }
 
     try {
-        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id})
+        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id })
 
         if (!task) {
             return res.status(404).send()
